edbe9108526097c15943779ea887c5a2cf73ace0
martinSternelius <martin.sternelius@gmail.com>
2010-12-03 13:20:58 +0100

    moving templates folder to correct location and fixing indentation of files

--- src/e_valuate/evaluate/views.py
+++ src/e_valuate/evaluate/views.py
@@ -1,14 +1,21 @@
 from django.template import loader, Context, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib import auth
 from evaluate.models import *
+from e_valuate.evaluate.models import Evaluation
+from e_valuate import evaluate
 
 def index(request):
-  t = loader.get_template('index.html')
-  c = RequestContext(request)
-  return HttpResponse(t.render(c))
+    t = loader.get_template('index.html')
+    c = RequestContext(request)
+    return HttpResponse(t.render(c))
 
 def logout(request):
-  auth.logout(request)
-  return HttpResponseRedirect('/')
+    auth.logout(request)
+    return HttpResponseRedirect('/')
+
+def questions(request, evaluation_id):
+    evaluation = get_object_or_404(Evaluation, pk=evaluation_id)
+    print evaluation.name
+    return render_to_response('evaluation/questions.html', {'evaluation': evaluation})
--- src/e_valuate/urls.py
+++ src/e_valuate/urls.py
@@ -5,13 +5,13 @@ admin.autodiscover()
 
 from e_valuate import evaluate
 from e_valuate.evaluate.views import index as evaluateindex
+import e_valuate.evaluate.views
 
-urlpatterns = patterns('',
-    
+urlpatterns = patterns('',    
     (r'^$', evaluateindex),
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', 'e_valuate.evaluate.views.logout'),
     (r'^admin/', include(admin.site.urls)),
     (r'^index/$', evaluateindex),
-    
+    (r'^evaluation/(?P<evaluation_id>\d+)/questions$', 'e_valuate.evaluate.views.questions'),
 )
